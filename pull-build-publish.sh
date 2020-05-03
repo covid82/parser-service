299f8d7f97338484befd393b7a660e8b6fe379a7
deniszjukow <denis.zjukow@gmail.com>
2020-05-03 20:37:52 +0200

    Added pull-build-publish.sh.

--- pull-build-publish.sh
+++ pull-build-publish.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 git pull
+set SBT_OPTS="-Xms512M -Xmx1024M -Xss2M -XX:MaxMetaspaceSize=1024M" sbt
+sbt assembly
 docker build -t parser-service .
 docker tag parser-service:latest 489683348645.dkr.ecr.eu-west-1.amazonaws.com/parser-service:latest
 docker push 489683348645.dkr.ecr.eu-west-1.amazonaws.com/parser-service:latest
