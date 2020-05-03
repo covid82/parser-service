d7010724486f3d740558aa103fb311ea509417bc
deniszjukow <denis.zjukow@gmail.com>
2020-05-03 20:35:05 +0200

    Added math-service.calculate call.

--- pull-build-publish.sh
+++ pull-build-publish.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 git pull
-docker build -t math-service .
-docker tag math-service:latest 489683348645.dkr.ecr.eu-west-1.amazonaws.com/math-service:latest
-docker push 489683348645.dkr.ecr.eu-west-1.amazonaws.com/math-service:latest
+docker build -t parser-service .
+docker tag parser-service:latest 489683348645.dkr.ecr.eu-west-1.amazonaws.com/parser-service:latest
+docker push 489683348645.dkr.ecr.eu-west-1.amazonaws.com/parser-service:latest
