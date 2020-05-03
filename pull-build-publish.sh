f2675975e97dbad23cac1962bda4b524ef049d17
deniszjukow <denis.zjukow@gmail.com>
2020-05-03 20:43:20 +0200

    Added pull-build-publish.sh.

--- pull-build-publish.sh
+++ pull-build-publish.sh
@@ -2,6 +2,7 @@
 git pull
 set SBT_OPTS="-Xms512M -Xmx1024M -Xss2M -XX:MaxMetaspaceSize=1024M" sbt
 sbt assembly
+sudo aws ecr get-login-password --region eu-west-1 | sudo docker login --username AWS --password-stdin 489683348645.dkr.ecr.eu-west-1.amazonaws.com/parser-service
 docker build -t parser-service .
 docker tag parser-service:latest 489683348645.dkr.ecr.eu-west-1.amazonaws.com/parser-service:latest
 docker push 489683348645.dkr.ecr.eu-west-1.amazonaws.com/parser-service:latest
